831a62a38ffad0b798be7fea20e81e5d67856e8a
Caen De Silva <caen@desilva.se>
2022-02-24 17:42:24 +0100

    Fix template JS errors and update mobile menu

--- resources/views/app.blade.php
+++ resources/views/app.blade.php
@@ -7,8 +7,6 @@
 	<meta name="theme-color" content="#000000" />
 	<link rel="shortcut icon" href="./assets/img/favicon.ico" />
 	<link rel="apple-touch-icon" sizes="76x76" href="./assets/img/apple-icon.png" />
-	<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/Chart.js/2.9.4/Chart.min.css" />
-	<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.3/css/all.min.css" />
 
 	<!-- Untill we have something stable enough to warrant compiling the tailwind ourselves -->
 	<link rel="stylesheet" href="https://unpkg.com/tailwindcss@1.4.6/dist/base.min.css">
@@ -32,7 +30,7 @@
 			scroll-behavior: smooth;
 		}
 		.table-of-contents {
-			list-style-type: "none";
+			margin-bottom: 1rem;
 		}
 		.table-of-contents ul {
 			margin-left: 1rem;
@@ -121,74 +119,30 @@
 			class="md:left-0 md:block md:fixed md:top-0 md:bottom-0 md:overflow-y-auto md:flex-row md:flex-nowrap md:overflow-hidden shadow-xl bg-white flex flex-wrap items-center justify-between relative md:w-64 lg:w-72 z-10 py-4 px-6">
 			<div
 				class="md:flex-col md:items-stretch md:min-h-full md:flex-nowrap px-0 flex flex-wrap items-center justify-between w-full mx-auto">
+				<a class="md:block text-left md:pb-2 text-blueGray-600 mr-0 inline-block  text-sm uppercase font-bold p-4 px-0"
+				href="index{{ $realtime == false ? '.html' : '' }}">
+				{{ config('app.name') }} Docs
+			</a>
 				<button
 					class="cursor-pointer text-black opacity-50 md:hidden px-3 py-1 text-xl leading-none bg-transparent rounded border border-solid border-transparent"
 					type="button" onclick="toggleNavbar('example-collapse-sidebar')">
-					<i class="fas fa-bars"></i></button>
-				<a class="md:block text-left md:pb-2 text-blueGray-600 mr-0 inline-block  text-sm uppercase font-bold p-4 px-0"
-					href="index{{ $realtime == false ? '.html' : '' }}">
-					{{ config('app.name') }} Docs
-				</a>
-				<ul class="md:hidden items-center flex flex-wrap list-none">
-					<li class="inline-block relative">
-						<a class="text-blueGray-500 block py-1 px-3" href="#pablo"
-							onclick="openDropdown(event,'notification-dropdown')"><i class="fas fa-bell"></i></a>
-						<div class="hidden bg-white text-base z-50 float-left py-2 list-none text-left rounded shadow-lg mt-1"
-							style="min-width: 12rem;" id="notification-dropdown">
-							<a href="#pablo"
-								class="text-sm py-2 px-4 font-normal block w-full whitespace-nowrap bg-transparent text-blueGray-700">Action</a><a
-								href="#pablo"
-								class="text-sm py-2 px-4 font-normal block w-full whitespace-nowrap bg-transparent text-blueGray-700">Another
-								action</a><a href="#pablo"
-								class="text-sm py-2 px-4 font-normal block w-full whitespace-nowrap bg-transparent text-blueGray-700">Something
-								else here</a>
-							<div class="h-0 my-2 border border-solid border-blueGray-100"></div>
-							<a href="#pablo"
-								class="text-sm py-2 px-4 font-normal block w-full whitespace-nowrap bg-transparent text-blueGray-700">Seprated
-								link</a>
-						</div>
-					</li>
-					<li class="inline-block relative">
-						<a class="text-blueGray-500 block" href="#pablo"
-							onclick="openDropdown(event,'user-responsive-dropdown')">
-							<div class="items-center flex">
-								<span
-									class="w-12 h-12 text-sm text-white bg-blueGray-200 inline-flex items-center justify-center rounded-full"><img
-										alt="..." class="w-full rounded-full align-middle border-none shadow-lg"
-										src="./assets/img/team-1-800x800.jpg" /></span>
-							</div>
-						</a>
-						<div class="hidden bg-white text-base z-50 float-left py-2 list-none text-left rounded shadow-lg mt-1"
-							style="min-width: 12rem;" id="user-responsive-dropdown">
-							<a href="#pablo"
-								class="text-sm py-2 px-4 font-normal block w-full whitespace-nowrap bg-transparent text-blueGray-700">Action</a><a
-								href="#pablo"
-								class="text-sm py-2 px-4 font-normal block w-full whitespace-nowrap bg-transparent text-blueGray-700">Another
-								action</a><a href="#pablo"
-								class="text-sm py-2 px-4 font-normal block w-full whitespace-nowrap bg-transparent text-blueGray-700">Something
-								else here</a>
-							<div class="h-0 my-2 border border-solid border-blueGray-100"></div>
-							<a href="#pablo"
-								class="text-sm py-2 px-4 font-normal block w-full whitespace-nowrap bg-transparent text-blueGray-700">Seprated
-								link</a>
-						</div>
-					</li>
-				</ul>
+					<svg xmlns="http://www.w3.org/2000/svg" height="24" viewBox="0 0 24 24" width="24"><path d="M0 0h24v24H0z" fill="none"/><path d="M3 18h18v-2H3v2zm0-5h18v-2H3v2zm0-7v2h18V6H3z"/></svg></button>
+			
 				<div class="md:flex md:flex-col md:items-stretch md:opacity-100 md:relative md:mt-4 md:shadow-none shadow absolute top-0 left-0 right-0 z-40 overflow-y-auto overflow-x-hidden h-auto items-center flex-1 rounded hidden"
 					id="example-collapse-sidebar">
 					<div class="md:min-w-full md:hidden block pb-4 mb-4 border-b border-solid border-blueGray-200">
 						<div class="flex flex-wrap">
-							<div class="w-6/12">
+							<div class="w-9/12">
 								<a class="md:block text-left md:pb-2 text-blueGray-600 mr-0 inline-block whitespace-nowrap text-sm uppercase font-bold p-4 px-0"
-									href="javascript:void(0)">
-									Tailwind Starter Kit
+									href="index{{ $realtime == false ? '.html' : '' }}">
+									{{ config('app.name') }} Docs
 								</a>
 							</div>
-							<div class="w-6/12 flex justify-end">
+							<div class="w-3/12 flex justify-end">
 								<button type="button"
 									class="cursor-pointer text-black opacity-50 md:hidden px-3 py-1 text-xl leading-none bg-transparent rounded border border-solid border-transparent"
 									onclick="toggleNavbar('example-collapse-sidebar')">
-									<i class="fas fa-times"></i>
+									<svg xmlns="http://www.w3.org/2000/svg" height="24" viewBox="0 0 24 24" width="24"><path d="M0 0h24v24H0z" fill="none"/><path d="M19 6.41L17.59 5 12 10.59 6.41 5 5 6.41 10.59 12 5 17.59 6.41 19 12 13.41 17.59 19 19 17.59 13.41 12z"/></svg>
 								</button>
 							</div>
 						</div>
@@ -256,8 +210,6 @@
 	</script>
 	
 
-	<script src="https://cdnjs.cloudflare.com/ajax/libs/Chart.js/2.9.4/Chart.min.js" charset="utf-8"></script>
-	<script src="https://unpkg.com/@popperjs/core@2.9.1/dist/umd/popper.min.js" charset="utf-8"></script>
 	<script type="text/javascript">
 		/* Sidebar - Side navigation menu on mobile/responsive mode */
 	function toggleNavbar(collapseID) {
@@ -267,224 +219,6 @@
 		document.getElementById(collapseID).classList.toggle("py-3");
 		document.getElementById(collapseID).classList.toggle("px-6");
 	}
-	/* Function for dropdowns */
-	function openDropdown(event, dropdownID) {
-		let element = event.target;
-		while (element.nodeName !== "A") {
-			element = element.parentNode;
-		}
-		var popper = Popper.createPopper(element, document.getElementById(dropdownID), {
-			placement: "bottom-end"
-		});
-		document.getElementById(dropdownID).classList.toggle("hidden");
-		document.getElementById(dropdownID).classList.toggle("block");
-	}
-	
-	
-	(function() {
-		/* Add current date to the footer */
-		document.getElementById("javascript-date").innerHTML = new Date().getFullYear();
-		/* Chart initialisations */
-		/* Line Chart */
-		var config = {
-			type: "line",
-			data: {
-				labels: [
-				"January",
-				"February",
-				"March",
-				"April",
-				"May",
-				"June",
-				"July"
-				],
-				datasets: [
-				{
-					label: new Date().getFullYear(),
-					backgroundColor: "#4c51bf",
-					borderColor: "#4c51bf",
-					data: [65, 78, 66, 44, 56, 67, 75],
-					fill: false
-				},
-				{
-					label: new Date().getFullYear() - 1,
-					fill: false,
-					backgroundColor: "#ed64a6",
-					borderColor: "#ed64a6",
-					data: [40, 68, 86, 74, 56, 60, 87]
-				}
-				]
-			},
-			options: {
-				maintainAspectRatio: false,
-				responsive: true,
-				title: {
-					display: false,
-					text: "Sales Charts",
-					fontColor: "white"
-				},
-				legend: {
-					labels: {
-						fontColor: "white"
-					},
-					align: "end",
-					position: "bottom"
-				},
-				tooltips: {
-					mode: "index",
-					intersect: false
-				},
-				hover: {
-					mode: "nearest",
-					intersect: true
-				},
-				scales: {
-					xAxes: [
-					{
-						ticks: {
-							fontColor: "rgba(255,255,255,.7)"
-						},
-						display: true,
-						scaleLabel: {
-							display: false,
-							labelString: "Month",
-							fontColor: "white"
-						},
-						gridLines: {
-							display: false,
-							borderDash: [2],
-							borderDashOffset: [2],
-							color: "rgba(33, 37, 41, 0.3)",
-							zeroLineColor: "rgba(0, 0, 0, 0)",
-							zeroLineBorderDash: [2],
-							zeroLineBorderDashOffset: [2]
-						}
-					}
-					],
-					yAxes: [
-					{
-						ticks: {
-							fontColor: "rgba(255,255,255,.7)"
-						},
-						display: true,
-						scaleLabel: {
-							display: false,
-							labelString: "Value",
-							fontColor: "white"
-						},
-						gridLines: {
-							borderDash: [3],
-							borderDashOffset: [3],
-							drawBorder: false,
-							color: "rgba(255, 255, 255, 0.15)",
-							zeroLineColor: "rgba(33, 37, 41, 0)",
-							zeroLineBorderDash: [2],
-							zeroLineBorderDashOffset: [2]
-						}
-					}
-					]
-				}
-			}
-		};
-		var ctx = document.getElementById("line-chart").getContext("2d");
-		window.myLine = new Chart(ctx, config);
-		
-		/* Bar Chart */
-		config = {
-			type: "bar",
-			data: {
-				labels: [
-				"January",
-				"February",
-				"March",
-				"April",
-				"May",
-				"June",
-				"July"
-				],
-				datasets: [
-				{
-					label: new Date().getFullYear(),
-					backgroundColor: "#ed64a6",
-					borderColor: "#ed64a6",
-					data: [30, 78, 56, 34, 100, 45, 13],
-					fill: false,
-					barThickness: 8
-				},
-				{
-					label: new Date().getFullYear() - 1,
-					fill: false,
-					backgroundColor: "#4c51bf",
-					borderColor: "#4c51bf",
-					data: [27, 68, 86, 74, 10, 4, 87],
-					barThickness: 8
-				}
-				]
-			},
-			options: {
-				maintainAspectRatio: false,
-				responsive: true,
-				title: {
-					display: false,
-					text: "Orders Chart"
-				},
-				tooltips: {
-					mode: "index",
-					intersect: false
-				},
-				hover: {
-					mode: "nearest",
-					intersect: true
-				},
-				legend: {
-					labels: {
-						fontColor: "rgba(0,0,0,.4)"
-					},
-					align: "end",
-					position: "bottom"
-				},
-				scales: {
-					xAxes: [
-					{
-						display: false,
-						scaleLabel: {
-							display: true,
-							labelString: "Month"
-						},
-						gridLines: {
-							borderDash: [2],
-							borderDashOffset: [2],
-							color: "rgba(33, 37, 41, 0.3)",
-							zeroLineColor: "rgba(33, 37, 41, 0.3)",
-							zeroLineBorderDash: [2],
-							zeroLineBorderDashOffset: [2]
-						}
-					}
-					],
-					yAxes: [
-					{
-						display: true,
-						scaleLabel: {
-							display: false,
-							labelString: "Value"
-						},
-						gridLines: {
-							borderDash: [2],
-							drawBorder: false,
-							borderDashOffset: [2],
-							color: "rgba(33, 37, 41, 0.2)",
-							zeroLineColor: "rgba(33, 37, 41, 0.15)",
-							zeroLineBorderDash: [2],
-							zeroLineBorderDashOffset: [2]
-						}
-					}
-					]
-				}
-			}
-		};
-		ctx = document.getElementById("bar-chart").getContext("2d");
-		window.myBar = new Chart(ctx, config);
-	})();
 	</script>
 </body>
 
