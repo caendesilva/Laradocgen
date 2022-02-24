b4d10851fbb0ca2c6f98614a6ce700ca6d43da11
Caen De Silva <caen@desilva.se>
2022-02-24 22:15:39 +0100

    Publish the assets

--- build.sh
+++ build.sh
@@ -1,4 +1,4 @@
-#!/bin/sh
+#!/bin/bash -x
 
 echo "Building package"
 
@@ -6,5 +6,9 @@ cp README.md resources/docs/readme.md
 cp LICENSE.md resources/docs/license.md
 cp CHANGELOG.md resources/docs/changelog.md
 cp CONTRIBUTING.md resources/docs/contributing.md
+
+echo "Publishing assets"
+
+php ../../../artisan vendor:publish --tag="docgen" --force
 
 echo "Done"
