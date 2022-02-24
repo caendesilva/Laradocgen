c00a31fe6885429c98422586d3352adada343aae
Caen De Silva <caen@desilva.se>
2022-02-24 17:30:23 +0100

    Update routes.php

--- src/routes.php
+++ src/routes.php
@@ -2,7 +2,14 @@
 
 use Caendesilva\Docgen\DocumentationController;
 
-Route::get('docs-master', [DocumentationController::class, 'index'])->name('docs.index');
-Route::get('docs-master/{slug}', function ($slug) {
-	return (new DocumentationController)->show($slug);
-})->name('docs.show');
+if (config('app.env') === 'local') {
+	Route::get('realtime-docs', function () {
+      	return redirect()->route('docs.realtime', ['slug' => 'index']);
+	});
+	Route::get('realtime-docs/{slug}', function ($slug = 'index') {
+		return (new DocumentationController)->show($slug, true);
+	})->name('docs.realtime');
+	Route::get('documentation-generator/{slug}', function ($slug) {
+		return (new DocumentationController)->show($slug);
+	})->name('docs.show');
+}
