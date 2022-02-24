9c1d3692b14506e143adb6fb0a1f699b95dc9751
Caen De Silva <caen@desilva.se>
2022-02-24 15:42:14 +0100

    Add static helpers

--- src/Docgen.php
+++ src/Docgen.php
@@ -2,7 +2,38 @@
 
 namespace Caendesilva\Docgen;
 
+use GrahamCampbell\Markdown\Facades\Markdown;
+
 class Docgen
 {
-    // Build your next great package.
+    public static function getMarkdownFromSlug(string $slug): string|bool {
+        try {
+            return (string) Markdown::convertToHtml(file_get_contents(resource_path() . '/docs/src/' . $slug . '.md'));
+        } catch (\Throwable $th) {
+            return false;
+        }
+    }
+
+    public static function getMarkdownFromSlugOrFail(string $slug): string
+    {
+        $markdown = self::getMarkdownFromSlug($slug);
+        if ($markdown) {
+            return $markdown;
+        }
+        abort(404);
+    }
+
+    public static function getMarkdownFileSlugsArray(): array
+    {
+        $files = [];
+    
+        foreach (glob(resource_path() . '/docs/src/*.md') as $filepath) {
+            $slug = basename($filepath, '.md');
+            // In future we check if in the index if the slug has an order, and if so we set the order as the array index key
+            $files[] = $slug;
+        }
+
+        return $files;
+    }
+
 }
