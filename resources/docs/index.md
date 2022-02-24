1d65a779192601727c3fe365005c289bd70c3526
Caen De Silva <caen@desilva.se>
2022-02-24 17:55:55 +0100

    Remove duplicate class and reorder image

--- resources/docs/index.md
+++ resources/docs/index.md
@@ -2,7 +2,10 @@
 
 This is the documentation frontpage
 
-You can reference images using ![Example Image](media/example-image.jpg "Image by Picjumbo.com")
+You can reference images using 
 ```markdown
 ![Example Image](media/example-image.jpg "Image by Picjumbo.com")
 ```
+![Example Image](media/example-image.jpg "Image by Picjumbo.com")
+
+If you are using the realtime preview you still need to run the build command if you add new images.
--- resources/views/app.blade.php
+++ resources/views/app.blade.php
@@ -150,7 +150,7 @@
 					<form class="mt-6 mb-4 md:hidden">
 						<div class="mb-3 pt-0">
 							<input type="text" placeholder="Search"
-								class="border-0 px-3 py-2 h-12 border border-solid  border-blueGray-500 placeholder-blueGray-300 text-blueGray-600 bg-white rounded text-base leading-snug shadow-none outline-none focus:outline-none w-full font-normal" />
+								class="px-3 py-2 h-12 border border-solid  border-blueGray-500 placeholder-blueGray-300 text-blueGray-600 bg-white rounded text-base leading-snug shadow-none outline-none focus:outline-none w-full font-normal" />
 						</div>
 					</form>
 					
